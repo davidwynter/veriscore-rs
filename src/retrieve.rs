edfff267d0c02f28a17e8f203a457e0a03e391d9
olddave <stpdave@gmail.com>
2025-08-09 11:17:27 +1000

    Added support for testing

--- src/retrieve.rs
+++ src/retrieve.rs
@@ -1,8 +1,10 @@
 use crate::{serper::Serper, types::*};
 use futures::{stream, StreamExt};
 use anyhow::Result;
+use crate::serper::Searcher;
 
-pub async fn retrieve_for_record(serp: &Serper, rec: ExtractedClaimsRecord, concurrency: usize) -> Result<EvidenceRecord> {
+pub async fn retrieve_for_record(serp: &dyn Searcher, rec: ExtractedClaimsRecord, concurrency: usize)
+    -> Result<EvidenceRecord> {
     let tasks = rec.all_claims.iter().map(|c| {
         let s = serp;
         async move { let hits = s.search(c).await; (c.clone(), hits) }
